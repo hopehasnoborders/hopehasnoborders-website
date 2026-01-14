6df5a1c3fe543e5b5a048eba45dd5c6294903426
rarebirdgames <austins@rarebirdgames.com>
2026-01-13 23:05:28 -0800

    chore: Update pathPrefix for production root deployment

--- .eleventy.js
+++ .eleventy.js
@@ -10,7 +10,7 @@ module.exports = function (eleventyConfig) {
     });
 
     return {
-        pathPrefix: "/hopehasnoborders/",
+        pathPrefix: "/",
         dir: {
             input: "src",
             output: "_site",
